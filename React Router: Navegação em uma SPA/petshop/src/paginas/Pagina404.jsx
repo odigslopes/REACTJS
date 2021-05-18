9896226dbb91a205f45dce859918010c3bd55465
Diego <diegolopes.vieira@hotmail.com>
2021-05-18 06:19:34 -0300

    Adicionando curso React Router

---
+++ React Router: Navegação em uma SPA/petshop/src/paginas/Pagina404.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import image from '../assets/img/doguito404.svg'
+import '../assets/css/404.css'
+
+const Pagina404 = () => {
+  return(
+    <main className="container flex flex--centro flex--coluna">
+      <img className="doguito-imagem"src={image} alt="ilutração doguito"/>
+      <p className="naoencontrado-texto">
+        Ops, essa página não existe!
+      </p>      
+    </main>
+  )
+  
+}
+
+export default Pagina404
+
